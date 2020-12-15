448dde18676639c5eb17486604673315fdba55d6
NWojt <n.wojtowicz098@gmail.com>
2020-12-15 22:53:21 +0100

    ER zagadnienie posrednika # changed graph component color

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Navbar} from "react-bootstrap";
 import CustomAlert from "./custom-components/CustomAlert";
-import SupplyAndDemandInput from "./custom-components/SupplyAndDemandInput";
 import GraphComponent from "./custom-components/GraphComponent";
+import Button from "react-bootstrap/Button";
+import SupplyAndDemandInput from "./custom-components/SupplyAndDemandInput";
 
 class App extends React.Component {
 
@@ -10,15 +11,59 @@ class App extends React.Component {
         super(props);
         this.state = {
             parametersFilled: false,
-            supplyEqualToDemand: false
-        }
+            supplyEqualToDemand: false,
+            supplier1: "",
+            supplier2: "",
+            recipient1: "",
+            recipient2: "",
+            supplier3: "",
+            supplier4: "",
+            supplier1fee: "",
+            supplier2fee: "",
+            recipient1fee: "",
+            recipient2fee: "",
+            supplier3fee: "",
+            supplier4fee: "",
+            sup1rec1cost: "",
+            sup1rec2cost: "",
+            sup2rec1cost: "",
+            sup2rec2cost: "",
+            sup3rec1cost: "",
+            sup3rec2cost: "",
+            sup4rec1cost: "",
+            sup4rec2cost: ""
+        };
+        this.InputRef = React.createRef();
+        this.isSupplyEqualToDemand = this.isSupplyEqualToDemand.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
     }
 
-    isSupplyEqualToDemand(supply, demand) {
-        return supply === demand;
+    isSupplyEqualToDemand() {
+        const suppliers = this.state.supplier1 + this.state.supplier2 + this.state.supplier3 + this.state.supplier4;
+        const recipinets = this.state.recipient1 + this.state.recipient2;
+
+        return suppliers === recipinets;
     }
 
-    getSuppliers() {
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.onInputChange();
+    }
+
+    onInputChange() {
+        const currentInput = this.InputRef.current;
+        this.setState(currentInput.state);
+        console.log(this.state);
+    }
+
+    calculate() {
+        const selelrs = [this.state.supplier1, this.state.supplier2, this.state.supplier3, this.state.supplier4];
+        const oneTimeFee = [this.state.supplier1fee, this.state.supplier2fee, this.state.supplier3fee, this.state.supplier4fee];
+        const transportcost = [[this.state.sup1rec1cost, this.state.sup1rec2cost], [this.state.sup2rec1cost, this.state.sup2rec2cost], [this.state.sup3rec1cost, this.state.sup3rec2cost],
+        [this.state.sup4rec1cost, this.state.sup4rec2cost]];
+        const buyers = [this.state.recipient1, this.state.recipient2];
+        const sellingPrice = [this.state.recipient1fee, this.state.recipient2fee];
+
+
 
     }
 
@@ -26,25 +71,34 @@ class App extends React.Component {
         return(
             <div>
                 <div>
-                <Navbar variant="dark" bg="dark">
-                    <Navbar.Brand style={{fontSize: '40px'}}>Zagadnienie pośrednika</Navbar.Brand>
-                    {this.state.parametersFilled && this.state.supplyEqualToDemand &&
-                    <CustomAlert message='Supply is not equal to demand'/>
-                    }
-                </Navbar>
+                    <Navbar variant="dark" bg="dark">
+                        <Navbar.Brand style={{fontSize: '40px'}}>Zagadnienie pośrednika</Navbar.Brand>
+                        {this.state.parametersFilled && this.state.supplyEqualToDemand &&
+                        <CustomAlert message='Supply is not equal to demand'/>
+                        }
+                    </Navbar>
                 </div>
-                <div style={{display: 'flex', flexDirection:'row', height: '100%'}}>
-                <div style={{display: 'flex', flexDirection: 'row', width: '100%', height:'100%', paddingLeft:'20px'}}>
-                    <SupplyAndDemandInput style={{display: 'flex',
-                        flexDirection:'row', float:'left', width:'50%'}}
-                                          onClick={() => this.isSupplyEqualToDemand()}/>
-                </div>
+                <div style={{display: 'flex', flexDirection: 'row', height: '100%'}}>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '100%',
+                        height: '100%',
+                        paddingLeft: '20px'
+                    }}>
+                        <p style={{fontSize: '30px', color: 'black'}}> Data</p>
+                        <SupplyAndDemandInput style={{
+                            display: 'flex',
+                            flexDirection: 'row', float: 'left', width: '50%'
+                        }} ref={this.InputRef}/>
+                        {this.isSupplyEqualToDemand() && <Button variant="primary">Submit</Button>}
+                    </div>
                     <div style={{display: 'flex', flexDirection: 'row', width: '100%'}}>
                         <GraphComponent/>
                     </div>
                 </div>
             </div>
-    );
+        );
     }
 }
 export default App
--- src/custom-components/SupplyAndDemandInput.js
+++ src/custom-components/SupplyAndDemandInput.js
@@ -1,46 +1,121 @@
 import React from "react";
 import {Form} from "react-bootstrap";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
-const SupplyAndDemandInput = (props) => {
-    const {onClick} = props;
-    const INPUT_PLACEHOLDER = "Value";
+export default class SupplyAndDemandInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            supplier1: "",
+            supplier2: "",
+            recipient1: "",
+            recipient2: "",
+            supplier3: "",
+            supplier4: "",
+            sup1rec1cost: "",
+            sup1rec2cost: "",
+            sup2rec1cost: "",
+            sup2rec2cost: "",
+            sup3rec1cost: "",
+            sup3rec2cost: "",
+            sup4rec1cost: "",
+            sup4rec2cost: ""
+        }
+    }
 
-    return (
-        <Form>
-            <Form.Label style={{fontSize: '30px', color: 'black'}}>Suppliers</Form.Label>
-            <Form.Row>
-                <Form.Group as={Col} controlId="supplier1">
-                    <Form.Label>Supplier 1 </Form.Label>
-                    <Form.Control placeHolder={INPUT_PLACEHOLDER}/>
-                </Form.Group>
-                <Form.Group as={Col} controlId="supplier2">
-                    <Form.Label>Supplier 2 </Form.Label>
-                    <Form.Control placeHolder={INPUT_PLACEHOLDER}/>
-                </Form.Group>
-            </Form.Row>
-            <Form.Label style={{fontSize: '30px', color: 'black'}}>Recipients</Form.Label>
-            <Form.Row>
-                <Form.Group as={Col} controlId="recipient1">
-                    <Form.Label>Recipient 1 </Form.Label>
-                    <Form.Control placeHolder={INPUT_PLACEHOLDER}/>
-                </Form.Group>
-                <Form.Group as={Col} controlId="recipient2">
-                    <Form.Label>Recipient 2 </Form.Label>
-                    <Form.Control placeHolder={INPUT_PLACEHOLDER}/>
-                </Form.Group>
-            </Form.Row>
-            <Form.Row>
-                <Form.Group as={Col} controlId="recipient3">
-                    <Form.Label>Recipient 3 </Form.Label>
-                    <Form.Control placeHolder={INPUT_PLACEHOLDER}/>
-                </Form.Group>
-                <Form.Group as={Col} controlId="recipient4">
-                    <Form.Label>Recipient 4 </Form.Label>
-                    <Form.Control placeHolder={INPUT_PLACEHOLDER}/>
-                </Form.Group>
-            </Form.Row>
-        </Form>
-    )};
+    INPUT_PLACEHOLDER = "Value";
+    COST_PLACEHOLDER = "cost";
 
-export default SupplyAndDemandInput;
+
+    render() {
+        return (
+            <Form>
+                <Form.Row>
+                    <Col>
+                    </Col>
+                    <Col>
+                    </Col>
+                    <Col>
+                        <Form.Label>Recipient 1 </Form.Label>
+                        <Form.Control placeholder={this.INPUT_PLACEHOLDER} onChange={event =>
+                            this.setState({recipient1: event.target.value})}/>
+                    </Col>
+                    <Col>
+                        <Form.Label>Recipient 2 </Form.Label>
+                        <Form.Control placeholder={this.INPUT_PLACEHOLDER} onChange={event =>
+                            this.setState({recipient2: event.target.value})}/>
+                    </Col>
+                </Form.Row>
+                <Form.Row>
+                    <Col>
+                        <Form.Label>Supplier 1</Form.Label>
+                    </Col>
+                    <Col style={{width: '20%'}}>
+                        <Form.Control type='text' placeholder={this.INPUT_PLACEHOLDER} onChange={event =>
+                            this.setState({supplier1: event.target.value})}/>
+                    </Col>
+                    <Col>
+                        <Form.Control type='text' placeholder={this.COST_PLACEHOLDER} onChange={event =>
+                            this.setState({sup1rec1cost: event.target.value})}/>
+                    </Col>
+                    <Col>
+                        <Form.Control type='text' placeholder={this.COST_PLACEHOLDER} onChange={event =>
+                            this.setState({supp1rec2cost: event.target.value})}/>
+                    </Col>
+                </Form.Row>
+            <Form.Row>
+                <Col>
+                    <Form.Label>Supplier 2</Form.Label>
+                </Col>
+                    <Col style={{width: '20%'}}>
+                        <Form.Control placeholder={this.INPUT_PLACEHOLDER} onChange={event =>
+                            this.setState({supplier2: event.target.value})}/>
+                    </Col>
+                <Col>
+                    <Form.Control type='text' placeholder={this.COST_PLACEHOLDER} onChange={event =>
+                        this.setState({supp2rec1cost: event.target.value})}/>
+                </Col>
+                <Col>
+                    <Form.Control type='text' placeholder={this.COST_PLACEHOLDER} onChange={event =>
+                        this.setState({supp2rec2cost: event.target.value})}/>
+                </Col>
+                </Form.Row>
+                <Form.Row>
+                    <Col>
+                        <Form.Label>Supplier 3</Form.Label>
+                    </Col>
+                    <Col style={{width: '20%'}}>
+                        <Form.Control placeholder={this.INPUT_PLACEHOLDER} onChange={event =>
+                            this.setState({supplier3: event.target.value})}/>
+                    </Col>
+                    <Col>
+                        <Form.Control type='text' placeholder={this.COST_PLACEHOLDER} onChange={event =>
+                            this.setState({supp3rec1cost: event.target.value})}/>
+                    </Col>
+                    <Col>
+                        <Form.Control type='text' placeholder={this.COST_PLACEHOLDER} onChange={event =>
+                            this.setState({supp3rec2cost: event.target.value})}/>
+                    </Col>
+                </Form.Row>
+                <Form.Row>
+                    <Col>
+                        <Form.Label>Supplier 4</Form.Label>
+                    </Col>
+                    <Col style={{width: '20%'}}>
+                        <Form.Control placeholder={this.INPUT_PLACEHOLDER} onChange={event =>
+                            this.setState({supplier4: event.target.value})}/>
+                    </Col>
+                    <Col>
+                        <Form.Control type='text' placeholder={this.COST_PLACEHOLDER} onChange={event =>
+                            this.setState({supp4rec1cost: event.target.value})}/>
+                    </Col>
+                    <Col>
+                        <Form.Control type='text' placeholder={this.COST_PLACEHOLDER} onChange={event =>
+                            this.setState({supp4rec2cost: event.target.value})}/>
+                    </Col>
+                </Form.Row>
+            </Form>
+        )
+    };
+}
